0fbb66d745492040ef9a114f0f1ceb4eb3835ff2
PST\muhammadizryzza <muhammadizryzza@commercedc.com.my>
2024-04-07 01:30:13 +0800

    7/4/2024

--- src/main/java/com/example/bas/api/controller/UserController.java
+++ src/main/java/com/example/bas/api/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.bas.api.controller;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,6 @@ public class UserController {
 
     private UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
